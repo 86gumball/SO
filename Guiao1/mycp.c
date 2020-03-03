e571b627c4419f54a9cf42727c54bc51bf10adc9
86gumball <lucasruben2000@gmail.com>
2020-03-03 20:03:25 +0000

    mycp now receives arguments

--- Guiao1/mycp.c
+++ Guiao1/mycp.c
@@ -5,17 +5,22 @@
 
 #define bufferSize 128
 
-int main(void){
+int main(int argc, char* argv[]){
     char buffer[bufferSize];
     int charsRead;
 
-    int source = open("testfile.txt", O_RDONLY);
+    if (argc != 3 | argv[1] == "--help") {
+        printf("Usage: ./mycp <source> <dest>\n");
+        return 0;
+    }
+
+    int source = open(argv[1], O_RDONLY);
     if (source == -1) {
         printf("Couldn't open source file\n");
         return -1;
     }
 
-    int dest = open("destfile.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    int dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
     if (dest == -1) {
         printf("Couldn't open dest file\n");
         close(source);
